e3a48b3458284b70e06466cded669fce11cadfb0
Sai Teja <your-github-kothurisaiteja1@gmail.com>
2026-08-21 21:43:10 +0530

    Update CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ app=FastAPI(
 )
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://studynest-byteja.vercel.app"
 ]
 
 app.add_middleware(
